ebfba3adb19b587b794cee743e5e251da35f7e29
Rodrigo Leite <rodrigoleite1073@gmail.com>
2022-09-30 22:21:03 -0500

    add grid to header and body

--- app/App.js
+++ app/App.js
@@ -3,11 +3,24 @@ import { ajax } from './helpers/ajax.js';
 import { Title } from './components/Title.js';
 import { Loader } from './components/Loader.js';
 import { Header } from './components/Header.js';
+import { Post } from './components/Posts.js';
+import { PostCard } from './components/PostCard.js';
 
 export function App() {
   const d = document,
     $root = d.getElementById('root');
 
   $root.appendChild(Header());
+  $root.appendChild(Post());
   $root.appendChild(Loader());
+
+  ajax({
+    url: api.POSTS,
+    cbSuccess: (posts) => {
+      let html = '';
+      posts.forEach((el) => (html += PostCard(el)));
+      d.querySelector('.loader').style.display = 'none';
+      d.getElementById('posts').innerHTML = html;
+    },
+  });
 }
