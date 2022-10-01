a2bd058d34409dc0a01c30212d665bfa80577d30
Rodrigo Leite <rodrigoleite1073@gmail.com>
2022-09-30 22:21:55 -0500

    add grid to header and body

---
+++ app/components/PostCard.js
@@ -0,0 +1,12 @@
+export function PostCard(props) {
+  return `
+  <article class="post-card">
+    <img src="https://placeimg.com/200/200/any" alt="any">
+    <h2>Titulo del post</h2>
+      <p>
+      <date datetime="">Fecha</date>
+      <a href="#">Ver publicación</a>
+      </p>
+    </article>
+    `;
+}
